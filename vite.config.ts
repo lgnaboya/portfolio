64783a14eb7687775273a154a75594d4cd811cdc
prettylittlebaby <144493425+lgnaboya@users.noreply.github.com>
2026-05-12 20:56:07 +0800

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // ✅ Add this line
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
